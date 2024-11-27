c435606e3a3cc6c32f796fa13a96490a1a5ca17e
Félix <71280649+FelixCalvo@users.noreply.github.com>
2024-11-27 18:41:51 +0100

    key setting

--- ahorcado.js
+++ ahorcado.js
@@ -1,12 +1,12 @@
 const nombres = ['Terremoto', 'Motocicleta', 'Edificio', 'Camiseta', 'Cerveza', 'Cocodrilo'];
 
 
-// Asocia el evento onclick a cada div
-document.querySelectorAll('.teclas').forEach(tecla => {
-  tecla.onclick = extraerNumeroClase();
-});
+// // Asocia el evento onclick a cada div
+// document.querySelectorAll('.teclas').forEach(tecla => {
+//   tecla.onclick = extraerNumeroClase();
+// });
 
 
-console.log(nombres);
+// console.log(nombres);
 
 // Función que se ejecuta al hacer click en una tecla
